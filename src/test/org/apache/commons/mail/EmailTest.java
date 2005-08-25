f3c608a292fffb4104430605834aa2f667868c16
Henning Schmiedehausen <henning@apache.org>
2005-08-25 09:40:24 +0000

    Gee, do you all have multi-GHz CPUs? This test fails consistently for me on a slow CPU... Add some fuzzyness to the time compare.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/email/trunk@240027 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/mail/EmailTest.java
+++ src/test/org/apache/commons/mail/EmailTest.java
@@ -1559,13 +1559,21 @@ public class EmailTest extends BaseEmailTestCase
     public void testGetSetSentDate()
     {
         // with input date
+
         Date dtTest = Calendar.getInstance().getTime();
         this.email.setSentDate(dtTest);
         assertEquals(dtTest, this.email.getSentDate());
 
         // with null input (this is a fudge :D)
         this.email.setSentDate(null);
-        assertEquals(dtTest, this.email.getSentDate());
+
+        Date sentDate = this.email.getSentDate();
+
+        // Date objects are millisecond specific. If you have a slow processor,
+        // time passes between the generation of dtTest and the new Date() in
+        // getSentDate() and this test fails. Make sure that the difference
+        // is less than a second...
+        assertTrue(Math.abs(sentDate.getTime() - dtTest.getTime()) < 1000);
     }
 
     /** */
