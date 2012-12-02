68c301c333f7f1baf3c8a20084d4afb1232d8319
Thomas Neidhart <tn@apache.org>
2012-12-02 19:25:13 +0000

    [EMAIL-117] Remove emulation support for nested exceptions.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/email/trunk@1416237 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/mail/EmailException.java
+++ src/java/org/apache/commons/mail/EmailException.java
@@ -23,9 +23,10 @@ import java.io.PrintWriter;
 /**
  * Exception thrown when a checked error occurs in commons-email.
  * <p>
- * Supports nesting, emulating JDK 1.4 behavior if necessary.
- * <p>
  * Adapted from FunctorException in Commons Collections.
+ * <p>
+ * Emulation support for nested exceptions has been removed in {@code Email 1.3},
+ * supported by JDK &ge; 1.4.
  *
  * @author jakarta-commons
  * @since 1.0
@@ -38,40 +39,12 @@ public class EmailException
     private static final long serialVersionUID = 5550674499282474616L;
 
     /**
-     * Does JDK support nested exceptions?
-     */
-    private static final boolean JDK_SUPPORTS_NESTED;
-
-    static
-    {
-        boolean flag = false;
-
-        try
-        {
-            Throwable.class.getDeclaredMethod("getCause", new Class[0]);
-            flag = true;
-        }
-        catch (NoSuchMethodException ex)
-        {
-            flag = false;
-        }
-
-        JDK_SUPPORTS_NESTED = flag;
-    }
-
-    /**
-     * Root cause of the exception
-     */
-    private final Throwable rootCause;
-
-    /**
      * Constructs a new <code>EmailException</code> with no
      * detail message.
      */
     public EmailException()
     {
         super();
-        this.rootCause = null;
     }
 
     /**
@@ -83,7 +56,6 @@ public class EmailException
     public EmailException(String msg)
     {
         super(msg);
-        this.rootCause = null;
     }
 
     /**
@@ -95,8 +67,7 @@ public class EmailException
      */
     public EmailException(Throwable rootCause)
     {
-        super((rootCause == null) ? null : rootCause.getMessage());
-        this.rootCause = rootCause;
+        super(rootCause);
     }
 
     /**
@@ -109,18 +80,7 @@ public class EmailException
      */
     public EmailException(String msg, Throwable rootCause)
     {
-        super(msg);
-        this.rootCause = rootCause;
-    }
-
-    /**
-     * Gets the cause of this throwable.
-     *
-     * @return  the cause of this throwable, or <code>null</code>
-     */
-    public Throwable getCause()
-    {
-        return rootCause;
+        super(msg, rootCause);
     }
 
     /**
@@ -158,12 +118,6 @@ public class EmailException
         synchronized (out)
         {
             super.printStackTrace(out);
-
-            if (!JDK_SUPPORTS_NESTED && (rootCause != null))
-            {
-                out.print("Caused by: ");
-                rootCause.printStackTrace(out);
-            }
         }
     }
 }
