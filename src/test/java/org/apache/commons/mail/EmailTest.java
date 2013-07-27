d32e057fbae52579747388aafc307a9899db0a19
Benedikt Ritter <britter@apache.org>
2013-07-27 09:24:44 +0000

    getHeaders returns a Map<String, String> - not need to call toString on a String object
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/email/trunk@1507612 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -910,7 +910,7 @@ public class EmailTest extends AbstractEmailTest
         
         assertTrue(email.getHeaders().size() == 1);
         // the header should not yet be folded -> will be done by buildMimeMessage()
-        assertFalse(email.getHeaders().get("X-LongHeader").toString().contains("\r\n"));
+        assertFalse(email.getHeaders().get("X-LongHeader").contains("\r\n"));
         
         email.buildMimeMessage();
 
