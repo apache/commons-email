9e9d1c24f9a7c0822e829fa4c25f1f029fe7a039
Gary Gregory <garydgregory@gmail.com>
2023-12-17 11:02:52 -0500

    Remove unused imports

--- src/main/java/org/apache/commons/mail/Email.java
+++ src/main/java/org/apache/commons/mail/Email.java
@@ -24,7 +24,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
--- src/main/java/org/apache/commons/mail/HtmlEmail.java
+++ src/main/java/org/apache/commons/mail/HtmlEmail.java
@@ -23,7 +23,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
