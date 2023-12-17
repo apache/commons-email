fceb90e9fce9a78cd27fbd09cab802bcc7e2714d
Gary Gregory <garydgregory@gmail.com>
2023-12-17 10:58:09 -0500

    Javadoc

--- src/main/java/org/apache/commons/mail/EmailUtils.java
+++ src/main/java/org/apache/commons/mail/EmailUtils.java
@@ -135,7 +135,7 @@ final class EmailUtils {
     /**
      * Checks if a String is empty ("") or null.
      * <p>
-     * Copied from Commons Lang v2.1, svn 240418
+     * Copied from Commons Lang 2.1, svn 240418
      * </p>
      *
      * @param str the String to check, may be null
@@ -148,7 +148,7 @@ final class EmailUtils {
     /**
      * Checks if a String is not empty ("") and not null.
      * <p>
-     * Copied from Commons Lang v2.1, svn 240418
+     * Copied from Commons Lang 2.1, svn 240418
      * </p>
      *
      * @param str the String to check, may be null
@@ -172,7 +172,7 @@ final class EmailUtils {
      * seed and using it for each call, the same random sequence of strings can be generated repeatedly and predictably.
      * </p>
      * <p>
-     * Copied from Commons Lang v2.1, svn 201930
+     * Copied from Commons Lang 2.1, svn 201930
      * </p>
      *
      * @param count   the length of random string to create
@@ -232,7 +232,7 @@ final class EmailUtils {
      * Characters will be chosen from the set of alphabetic characters.
      * </p>
      * <p>
-     * Copied from Commons Lang v2.1, svn 201930
+     * Copied from Commons Lang 2.1, svn 201930
      * </p>
      *
      * @param count the length of random string to create
