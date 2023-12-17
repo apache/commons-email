b63534650ce3e8ccc34c5ab1dc1979e44467f058
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2023-12-17 16:21:11 +0100

    create jakarta module

---
+++ commons-email-jakarta/src/test/java/org/apache/commons/mail/InvalidAddressTest.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.mail;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.IntStream;
+
+import org.apache.commons.mail.mocks.MockEmailConcrete;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test case for invalid Addresses in Email Class
+ *
+ * @since 1.0
+ */
+public class InvalidAddressTest extends AbstractEmailTest {
+
+    // @formatter:off
+    private static final String[] ARR_INVALID_EMAILS = {
+            "local name@domain.com",
+            "local(name@domain.com",
+            "local)name@domain.com",
+            "local<name@domain.com",
+            "local>name@domain.com",
+            "local,name@domain.com",
+            "local;name@domain.com",
+            "local:name@domain.com",
+            "local[name@domain.com",
+            "local]name@domain.com",
+            // "local\\name@domain.com", is considered valid for mail-1.4.1
+            "local\"name@domain.com",
+            "local\tname@domain.com",
+            "local\nname@domain.com",
+            "local\rname@domain.com",
+            "local.name@domain com",
+            "local.name@domain(com",
+            "local.name@domain)com",
+            "local.name@domain<com",
+            "local.name@domain>com",
+            "local.name@domain,com",
+            "local.name@domain;com",
+            "local.name@domain:com",
+            // "local.name@domain[com",
+            "local.name@domain]com",
+            "local.name@domain\\com",
+            "local.name@domain\tcom",
+            "local.name@domain\ncom",
+            "local.name@domain\rcom",
+            "local.name@",
+            "@domain.com" };
+    // @formatter:on
+
+    private MockEmailConcrete email;
+
+    @BeforeEach
+    public void setUpInvalidAddressTest() {
+        // reusable objects to be used across multiple tests
+        email = new MockEmailConcrete();
+    }
+
+    @Test
+    public void testAddInvalidBcc() throws Exception {
+        // Test adding invalid 'BCC' addresses
+        // @formatter:off
+        IntStream.range(0, ARR_INVALID_EMAILS.length).forEach(i -> assertThrows(EmailException.class,
+                () -> email.addBcc(ARR_INVALID_EMAILS[i], "Joe"),
+                () -> "addBcc " + i + " passed: " + ARR_INVALID_EMAILS[i]));
+        // @formatter:on
+    }
+
+    @Test
+    public void testAddInvalidCc() throws Exception {
+        // Test adding invalid 'CC' addresses
+        // @formatter:off
+        IntStream.range(0, ARR_INVALID_EMAILS.length).forEach(i -> assertThrows(EmailException.class,
+                () -> email.addCc(ARR_INVALID_EMAILS[i], "Joe"),
+                () -> "addCc " + i + " passed: " + ARR_INVALID_EMAILS[i]));
+        // @formatter:on
+    }
+
+    @Test
+    public void testAddInvalidTo() throws Exception {
+        // Test adding invalid 'to' addresses
+        // @formatter:off
+        IntStream.range(0, ARR_INVALID_EMAILS.length).forEach(i -> assertThrows(EmailException.class,
+                () -> email.addTo(ARR_INVALID_EMAILS[i], "Joe"),
+                () -> "addTo " + i + " passed: " + ARR_INVALID_EMAILS[i]));
+        // @formatter:on
+    }
+
+    @Test
+    public void testSetInvalidFrom() throws Exception {
+        // Test setting invalid 'from' addresses
+        // @formatter:off
+        IntStream.range(0, ARR_INVALID_EMAILS.length).forEach(i -> assertThrows(EmailException.class,
+                () -> email.setFrom(ARR_INVALID_EMAILS[i], "Joe"),
+                () -> "setFrom " + i + " passed: " + ARR_INVALID_EMAILS[i]));
+        // @formatter:on
+    }
+}
---
+++ commons-email-jakarta/src/test/java/org/apache/commons/mail/InvalidInternetAddressTest.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.mail;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
+
+import javax.mail.internet.InternetAddress;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test case demonstrating InternetAddress validation.
+ *
+ * @since 1.0
+ */
+public class InvalidInternetAddressTest extends AbstractEmailTest {
+    /** */
+    private static final String VALID_QUOTED_EMAIL = "\"John O'Groats\"@domain.com";
+
+    // @formatter:off
+    private static final String[] ARR_INVALID_EMAILS = {
+            "local name@domain.com",
+            "local(name@domain.com",
+            "local)name@domain.com",
+            "local<name@domain.com",
+            "local>name@domain.com",
+            "local,name@domain.com",
+            "local;name@domain.com",
+            "local:name@domain.com",
+            "local[name@domain.com",
+            "local]name@domain.com",
+            // "local\\name@domain.com", -- works for javamail-1.4.4
+            // "local\"name@domain.com", -- works for javamail-1.4.4
+            "local\tname@domain.com",
+            "local\nname@domain.com",
+            "local\rname@domain.com",
+            "local.name@domain com",
+            "local.name@domain(com",
+            "local.name@domain)com",
+            "local.name@domain<com",
+            "local.name@domain>com",
+            "local.name@domain,com",
+            "local.name@domain;com",
+            "local.name@domain:com",
+            // "local.name@domain[com", -- works for javamail-1.5.5
+            "local.name@domain]com",
+            "local.name@domain\\com",
+            "local.name@domain\tcom",
+            "local.name@domain\ncom",
+            "local.name@domain\rcom",
+            "local.name@",
+            "@domain.com" };
+    // @formatter:on
+
+    @Test
+    public void testStrictConstructor() throws Exception {
+        // Prove InternetAddress constructor is throwing exception.
+
+        // test Invalid Email addresses
+        // @formatter:off
+        IntStream.range(0, ARR_INVALID_EMAILS.length).forEach(i -> assertThrows(Exception.class,
+                () -> new InternetAddress(ARR_INVALID_EMAILS[i]),
+                () -> "Strict " + i + " passed: " + ARR_INVALID_EMAILS[i]));
+        // @formatter:on
+
+        // test valid 'quoted' Email addresses
+        // Create Internet Address using "strict" constructor
+        assertDoesNotThrow(() -> new InternetAddress(VALID_QUOTED_EMAIL), () -> "Valid Quoted Email failed: " + VALID_QUOTED_EMAIL);
+    }
+
+    @Test
+    public void testValidateMethod() throws Exception {
+        // Prove InternetAddress constructor isn't throwing exception and
+        // the validate() method is
+
+        for (int i = 0; i < ARR_INVALID_EMAILS.length; i++) {
+
+            final InternetAddress address = new InternetAddress(ARR_INVALID_EMAILS[i], "Joe");
+
+            // N.B. validate() doesn't check addresses containing quotes or '['
+            final boolean quoted = ARR_INVALID_EMAILS[i].contains("\"");
+            final int atIndex = ARR_INVALID_EMAILS[i].indexOf("@");
+            final boolean domainBracket = atIndex >= 0 && ARR_INVALID_EMAILS[i].indexOf("[", atIndex) >= 0;
+            try {
+                address.validate();
+
+                if (!(quoted || domainBracket)) {
+                    fail("Validate " + i + " passed: " + ARR_INVALID_EMAILS[i]);
+                }
+            } catch (final Exception ex) {
+                if (quoted || domainBracket) {
+                    fail("Validate " + i + " failed: " + ARR_INVALID_EMAILS[i] + " - " + ex.getMessage());
+                }
+            }
+        }
+
+        // test valid 'quoted' Email addresses
+        assertDoesNotThrow(() -> new InternetAddress(VALID_QUOTED_EMAIL, "Joe").validate(), () -> "Valid Quoted Email failed: " + VALID_QUOTED_EMAIL);
+    }
+
+    @Test
+    public void testValidateMethodCharset() throws Exception {
+        // Prove InternetAddress constructor isn't throwing exception and
+        // the validate() method is
+
+        for (int i = 0; i < ARR_INVALID_EMAILS.length; i++) {
+
+            final InternetAddress address = new InternetAddress(ARR_INVALID_EMAILS[i], "Joe", StandardCharsets.UTF_8.name());
+
+            // N.B. validate() doesn't check addresses containing quotes or '['
+            final boolean quoted = ARR_INVALID_EMAILS[i].contains("\"");
+            final int atIndex = ARR_INVALID_EMAILS[i].indexOf("@");
+            final boolean domainBracket = atIndex >= 0 && ARR_INVALID_EMAILS[i].indexOf("[", atIndex) >= 0;
+
+            try {
+                address.validate();
+                if (!(quoted || domainBracket)) {
+                    fail("Validate " + i + " passed: " + ARR_INVALID_EMAILS[i]);
+                }
+
+            } catch (final Exception ex) {
+                if (quoted || domainBracket) {
+                    fail("Validate " + i + " failed: " + ARR_INVALID_EMAILS[i] + " - " + ex.getMessage());
+                }
+            }
+
+        }
+
+        // test valid 'quoted' Email addresses
+        assertDoesNotThrow(() -> new InternetAddress(VALID_QUOTED_EMAIL, "Joe", StandardCharsets.UTF_8.name()).validate(),
+                () -> "Valid Quoted Email failed: " + VALID_QUOTED_EMAIL);
+
+    }
+
+}
