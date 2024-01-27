b66e6a33a246db8d25e255f4bbfd735b84eeaa58
Gary Gregory <garydgregory@gmail.com>
2024-01-27 09:02:42 -0500

    Whitespace

--- commons-email2-javax/src/main/java/org/apache/commons/mail2/javax/package-info.java
+++ commons-email2-javax/src/main/java/org/apache/commons/mail2/javax/package-info.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * Commons-Email aims to provide a API for sending email. It is built on top of the Java Mail API, which it aims to simplify. All help is gratefully received.
  */
