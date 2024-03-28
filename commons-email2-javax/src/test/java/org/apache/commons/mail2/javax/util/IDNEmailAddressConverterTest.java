d0f2257ec3a6c4521c7c28d3c2facd3e0312119c
Gary Gregory <garydgregory@gmail.com>
2024-03-28 18:56:28 -0400

    Add missing import

--- commons-email2-javax/src/test/java/org/apache/commons/mail2/javax/util/IDNEmailAddressConverterTest.java
+++ commons-email2-javax/src/test/java/org/apache/commons/mail2/javax/util/IDNEmailAddressConverterTest.java
@@ -17,6 +17,7 @@
 package org.apache.commons.mail2.javax.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import javax.mail.internet.InternetAddress;
 
